e96520341f1a1832fec911a6b5d8098e67a9de5d
MakeevVsevolod <banderolman@mail.ru>
2023-10-16 23:49:06 +0600

    Create СМЕНАЗНАКАиПРОЧ-4.cs

---
+++ алгоритмизация/СМЕНАЗНАКАиПРОЧ/СМЕНАЗНАКАиПРОЧ-4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* для n эликов 4) определить минимальный размер под последовательности из отрицательных, если не одной под последовательности - выдать сообщение*/
+
+
+        }
+    }
+}
