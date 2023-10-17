fb11c3b182135c6e1b17ddf9f43c43984065c3b7
MakeevVsevolod <banderolman@mail.ru>
2023-10-17 12:55:40 +0600

    Update СМЕНАЗНАКАиПРОЧ-4.cs

--- алгоритмизация/СМЕНАЗНАКАиПРОЧ/СМЕНАЗНАКАиПРОЧ-4.cs
+++ алгоритмизация/СМЕНАЗНАКАиПРОЧ/СМЕНАЗНАКАиПРОЧ-4.cs
@@ -10,8 +10,56 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            /* для n эликов 4) определить минимальный размер под последовательности из отрицательных, если не одной под последовательности - выдать сообщение*/
+            /* для n эликов 4) определить минимальный размер под последовательности из отрицательных, 
+             * если не одной под последовательности - выдать сообщение*/
 
+            Console.Write("Количество элементов n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("элементы через enter: ");
+
+            int a = 0;
+            int negative = 0;
+            int check = 0;
+
+            int minnegative = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+
+                if (a<0)
+                {
+                    negative = negative+1;
+                    check = 1;
+                }
+                else
+                {
+                    if ((negative < minnegative) && (negative != 0))
+                    {
+                        minnegative = negative;
+                    }
+                    negative = 0;
+                }
+
+                if (i==n-1)
+                {
+                    if (negative < minnegative)
+                    {
+                        minnegative = negative;
+                    }
+                    negative = 0;
+                }
+
+            }
+
+            if (check==0)
+            {
+                Console.WriteLine("ашибачка отрицательных нет(((");
+            }
+            else
+            {
+                Console.WriteLine($"минимальный ряд отрицательных: {minnegative}");
+            }
 
         }
     }
