2e111da42896dcdb695d4073a6d907ec5dcb2059
MakeevVsevolod <banderolman@mail.ru>
2024-05-28 01:06:14 +0600

    Create phones_hashtable.cs

---
+++ алгоритмизация/2сем/phones_hashtable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfDigitsEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 222, 54, 78, 101, 1245, 1474, 427, 7852, 0 };
+
+            var numbersWithEvenSumOfDigits = from n in numbers
+                                             where n.ToString().Sum(c => int.Parse(c.ToString())) % 2 == 0
+                                             select n;
+
+            Console.WriteLine("Элементы с чётной суммой цифр:");
+            foreach (int n in numbersWithEvenSumOfDigits)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
